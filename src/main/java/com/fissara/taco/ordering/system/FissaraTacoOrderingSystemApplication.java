0613456d9f2c18e05803fffad55958d259209e8e
rnl2004 <rnl2004@gmail.com>
2020-04-02 23:51:28 +0800

    initial commit

---
+++ src/main/java/com/fissara/taco/ordering/system/FissaraTacoOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fissara.taco.ordering.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FissaraTacoOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FissaraTacoOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fissara/taco/ordering/system/FissaraTacoOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fissara.taco.ordering.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FissaraTacoOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
